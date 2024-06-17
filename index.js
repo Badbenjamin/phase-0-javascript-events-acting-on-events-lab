e196e2d6e1276eb2c9416f9b19cfbc64eab8a980
ben mccarthy <bmccarthy90@gmail.com>
2024-06-17 18:24:06 -0400

    passing dodger event code

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+// find the element you want to modify and assign it a variable
+const dodger = document.getElementById("dodger");
+
+// create a function that modifies that variable
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+// create an event listener that calls the function
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left <= 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
